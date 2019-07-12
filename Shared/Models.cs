a4bdb46501c1a6e41854895471cdf9fc359a596f
Ajith R <ajithr@syncfusion.com>
2019-07-12 12:23:54 +0530

    Added blazor generic type sample

---
+++ Shared/Models.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericTypeBlazor.Shared.Models
+{
+    public class EventArgsData<T>
+    {
+        public bool IsRendered { get; set; }
+
+        public T Data { get; set; }
+    }
+
+    public class EventArguments
+    {
+        public bool IsRendered { get; set; }
+
+        public object Data { get; set; }
+    }
+}
